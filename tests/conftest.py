944f1456391fe194e4635d39c09cbd466a649422
José Padilla <jpadilla@webapplicate.com>
2014-09-03 15:59:59 -0700

    Add tests for Django 1.7

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import os
+
+import django
 from django.conf import settings
 
 
 def pytest_configure():
     if not settings.configured:
         os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+    try:
+        django.setup()
+    except AttributeError:
+        pass
